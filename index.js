e9c6f121358961dd8ff2f943178ff1aca61831c3
TekleJankhoteli <jankhotelitekla@gmail.com>
2023-07-16 20:26:25 +0400

    last commit

--- index.js
+++ index.js
@@ -1,62 +1,27 @@
-// *Change the background color of an HTML element.
 
-let h1=document.querySelector(".saxeli");
-h1.style.backgroundColor="pink";
 
-// *Hide an HTML element by changing its display property.
 
-let h2=document.querySelector(".email")
-h2.style.display="none";
+const btn=document.querySelector("button")
+const h1=document.querySelector("h1")
+const img=document.querySelector("img")
 
-// *Toggle a CSS class on an element when it is clicked.
-
-let btn=document.querySelector("#btn");
-let paragraph=document.querySelector("#paragraph")
-
-btn.addEventListener("click", () =>{
-    if(paragraph.style.display==="none"){
-        paragraph.style.display="block";
-    }else{
-        paragraph.style.display="none"
-    }
-
+btn.addEventListener("click", ()=>{
+  fetchCat();
 })
 
-// *Change the text content of an element when a button is clicked.
-
-let button=document.querySelector("#button")
-let h3=document.querySelector(".h3")
-
-button.addEventListener("click",()=> {
-    h3.innerHTML="this is true text"
-})
+async function fetchCat(){
+  let res=await fetch("https://catfact.ninja/fact")
+  let data=await res.json();
+  console.log(data)
+  h1.innerHTML=data.fact;
 
 
-// *Increment or decrement a counter when respective buttons are clicked.
+let imgRes= await fetch("https://api.thecatapi.com/v1/images/search");
+let imgData= await imgRes.json();
+console.log(imgData);
 
-let inc=document.querySelector("#inc");
-let dec=document.querySelector("#dec");
-let numb=document.querySelector("#numb");
-
-let count=0;
-
-inc.addEventListener("click",()=>{
-    numb.innerHTML=count++
-})
-
-dec.addEventListener("click",()=>{
-    numb.innerHTML=count--
-})
-
-
-// *change box color to click the button
-
-let box=document.querySelector(".box")
-let cbc=document.querySelector(".CBC")
-
-cbc.addEventListener("click", ()=>{
-    box.style.backgroundColor="blue"
-})
-
-
-
+let catImglink=imgData[0].url;
+  img.style.display="block";
+  img.src=catImglink
+  
+}
